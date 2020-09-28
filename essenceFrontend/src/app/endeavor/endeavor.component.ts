291871376e6a2c9c2e0f777e8856426669e611c8
myerd <santaali1@gmail.com>
2020-09-28 16:39:09 +0300

    Initial commit

---
+++ essenceFrontend/src/app/endeavor/endeavor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-endeavor',
+  templateUrl: './endeavor.component.html',
+  styleUrls: ['./endeavor.component.scss']
+})
+export class EndeavorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ essenceFrontend/src/app/softwaresystem/softwaresystem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-softwaresystem',
+  templateUrl: './softwaresystem.component.html',
+  styleUrls: ['./softwaresystem.component.scss']
+})
+export class SoftwaresystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
